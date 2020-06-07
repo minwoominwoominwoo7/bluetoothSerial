85eba12d91943f956cb40a4bcf1abf9f2df7a842
minwoominwoominwoo7 <57865722+minwoominwoominwoo7@users.noreply.github.com>
2020-06-07 21:58:06 +0900

    Add files via upload

---
+++ ADC_mody.ino
@@ -0,0 +1,44 @@
+/* 
+ * rosserial ADC Example
+ * 
+ * This is a poor man's Oscilloscope.  It does not have the sampling 
+ * rate or accuracy of a commerical scope, but it is great to get
+ * an analog value into ROS in a pinch.
+ */
+
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+#endif
+#include <ros.h>
+#include <rosserial_arduino/Adc.h>
+
+ros::NodeHandle nh;
+
+rosserial_arduino::Adc adc_msg;
+ros::Publisher p("adc", &adc_msg);
+
+void setup()
+{ 
+  pinMode(13, OUTPUT);
+  nh.initNode();
+
+  nh.advertise(p);
+}
+
+int potVal;
+int vol;
+
+void loop()
+{
+  adc_msg.adc0 = vol ;
+
+  potVal = analogRead(A0);
+  vol = map(potVal, 0, 1023,0,100);
+  //Serial.println(vol);
+    
+  p.publish(&adc_msg);
+
+  nh.spinOnce();
+}
